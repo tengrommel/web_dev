6ccfdd61dd386141f1b6701fbe3cc0847c354d91
tengrommel <wwwsousoucom@126.com>
2018-02-28 23:46:15 +0800

    signin

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
-var database = [
-  {
-    username: "andrei",
-    password: "supersecret"
-  }
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript!",
+    "eat healthy!"
 ];
 
-var newsFeed = [
-  {
-    username: "Bobby",
-    timeline: "So tired from all that learning!"
-  },
-  {
-    username: "Sally",
-    timeline: "Javascript is sooooo cooool!"
-  }
-]
+var todosLength = todos.length
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
+// for (var i=0; i < todosLength; i++) {
+//   todos.pop();
+// }
+//
+// var counterOne = 10;
+// while (counterOne > 0) {
+//     console.log(counterOne);
+//     counterOne--
+// }
 
-function signIn(user, pass) {
-  if(user===database[0].username &&
-      pass === database[0].password) {
-    console.log(newsFeed);
-  }else {
-    alert("Sorry, wrong username and password!");
-  }
-}
+var counterTwo = 10
+do {
+    console.log(counterTwo);
+    counterTwo--;
+} while (counterTwo >0)
 
-signIn(userNamePrompt, passwordPrompt);
+todos.forEach(function (value, i) {
+    console.log(value, i)
+})
