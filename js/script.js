e479e2e62ab15966a37cb0673a99353bad4ad3bc
tengrommel <wwwsousoucom@126.com>
2018-02-28 22:30:20 +0800

    object and list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-var list = ["tiger", "cat", "bear", "bird"];
-
 var user = {
   name: "John",
   age: 34,
   hobby: "Soccer",
   isMarried: false,
+  spells: ["abrakadra", "shazam", "boo"],
   shout: function () {
-    console.log("ADDDDDDDDDD")
+    console.log("AHHHHHHHHHHHH!");
   }
-}
+};
+
+var list = [
+  {
+    username: "andy",
+    password: "secre"
+  },
+  {
+    username: "jess",
+    password: "123"
+  }
+];
+
